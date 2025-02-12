488446429715d82ab48a34a6ab94d9de0cc6dcb6
dan1kali <dl74@rice.edu>
2025-02-12 13:34:45 -0600

    declared pins, triggered ultrasonic waveform and reception

---
+++ UltrasoundSensor/UltrasoundSensor.ino
@@ -0,0 +1,26 @@
+
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  
+}
